ba715fa1d9fa05a8bcd3149abb3fce706d8b73c3
Jack Williams <j.williams@ou.edu>
2020-04-14 13:10:09 -0500

    fixed blues, pentatonic, chromatic scales

--- src/staff.js
+++ src/staff.js
@@ -16,12 +16,12 @@ class note {
     align; // positive or negative number
 
     // defaults, just do note('C4',0,'#',0) to overwrite
-    constructor(name, sfn=0, staff=0, align=0) {
-        this.name=name;
-        this.index=note_map[name];
-        this.sfn=sfn;
-        this.staff=staff;
-        this.align=align;
+    constructor(name, sfn=0, staff=0, align=0, index=0) {
+        this.name = name;
+        this.index = (index === 0) ? note_map[name] : index;
+        this.sfn = sfn;
+        this.staff = staff;
+        this.align = align;
     }
 }
 
@@ -57,26 +57,27 @@ const minor = [
     new note('F5'),
     new note('G5'),
     new note('A5'),
-    new note('B5', 'n'),
     new note('Bb5', 'b'),
+    new note('B5', 'n'),
     new note('C6')
 ];
 const blues = [
     new note('C4'),
     new note('D4'),
-    new note('Eb4', 0, 'b'),
+    new note('Eb4'),// 0, 'b'),
     new note('F4', 'n'),
     new note('F#4', '#'),
     new note('G4'),
-    new note('Bb4', 'b'),
+    new note(0, 0, 'b', 2, 5),
+    new note('Bb4', 'b', 'b', 0),
     new note('B4', 'n'),
     new note('C5'),
-    new note('Eb5'),
+    new note('Eb5', 0, 'b', 1),
     new note('F5', 'n'),
     new note('F#5', '#'),
     new note('G5'),
-    new note('B5', 'n'),
     new note('Bb5', 'b'),
+    new note('B5', 'n'),
     new note('C6')
 ];
 const pentatonic = [
@@ -92,46 +93,34 @@ const pentatonic = [
     new note('A5'),
     new note('C6')
 ];
-// {
-//     scale : ['C4','D4','E4','G4','A4','C5','D5','E5','G5','A5','C6'],
-//     sfn :   [0,0,0,0,0,0,0,0,0,0],
-//     staff : [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-// }
 const chromatic = [
-    new note('C4'),
-    new note('C#4'),
+    new note('C4', 'n'),
+    new note('C#4', '#'),
     new note('D4'),
-    new note('Eb4'),
-    new note('E4'),
-    new note('F4'),
-    new note('F#4'),
+    new note('Eb4', 'b'),
+    new note('E4', 'n'),
+    new note('F4', 'n'),
+    new note('F#4', '#'),
     new note('G4'),
-    new note('Ab4'),
-    new note('A4'),
-    new note('Bb4'),
-    new note('B4'),
-    new note('C5'),
-    new note('C#5'),
+    new note('Ab4', 'b'),
+    new note('A4', 'n'),
+    new note('Bb4', 'b'),
+    new note('B4', 'n'),
+    new note('C5', 'n'),
+    new note('C#5', '#'),
     new note('D5'),
-    new note('E5'),
-    new note('Eb5'),
-    new note('F5'),
-    new note('F#5'),
+    new note('E5', 'n'),
+    new note('Eb5', 'b'),
+    new note('F5', 'n'),
+    new note('F#5', '#'),
     new note('G5'),
-    new note('Ab5'),
-    new note('A5'),
-    new note('Bb5'),
-    new note('B5'),
-    new note('C6'),
-    new note('C#6'),
+    new note('Ab5', 'b'),
+    new note('A5', 'n'),
+    new note('Bb5', 'b'),
+    new note('B5', 'n'),
+    new note('C6', 'n')
+    // new note('C#6', '#'),
 ];
-// {
-//     scale : ['C4','C#4','D4','Eb4','E4','F4','F#4','G4','Ab4','A4','Bb4','B4',
-// 'C5', 'C#5', 'D5', 'Eb5', 'E5', 'F5', 'F#5', 'G5', 'Ab5', 'A5', 'Bb5', 'B5','C6'],
-//     sfn : ['\u266E','\u266F',0,'\u266D','\u266E','\u266E','\u266F',0,'\u266D','\u266E','\u266D','\u266E',
-// '\u266E','\u266F',0,'\u266D','\u266E','\u266E','\u266F',0,'\u266D','\u266E','\u266D','\u266E',0],
-//     staff : [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-// }
 
 function Note(props) {
     return (
@@ -198,71 +187,84 @@ function Lines(props) {
         // bottom marker
         ctx.fillRect(115, props.height - props.gap/2, 50, props.fill);
 
-        var y_ind = 0, x, y, nx = 15, ny=10;
+        var x, y, nx = 15, ny=10, j, index = 0;
+        start = props.height - props.gap/2
         // draw the notes
-        for (i in props.notes) {
-            y_ind = props.notes[i].index;
+        var length = (props.range === 2) ? props.notes.length : props.notes.length / 2;
+        for (i = 0; i < length; ++i) {
+            j = props.notes[i].index;
+            if (props.notes[i].name !== 0) {
+                // note location
+                x = 140 + (2*nx+5)*index;
+                y = props.height - (props.gap/2)*(j+1)
 
-            // note location
-            x = 140 + (2*nx+5)*i;
-            y = props.height - (props.gap/2)*(y_ind+1)
+                // draw the note
+                ctx.beginPath();
+                ctx.ellipse(x, y, nx, ny, 0, 0, Math.PI*2);
+                ctx.fill();
 
-            // draw the note
-            ctx.beginPath();
-            ctx.ellipse(x, y, nx, ny, 0, 0, Math.PI*2);
-            ctx.fill();
 
-            // add the label (if needed)
-            if (props.notes[i].sfn) {
-                ctx.fillStyle = '#000';
-                if (props.notes[i].sfn === "#")
-                    ctx.fillText('\u266F', x-5, y+6);
-                else if (props.notes[i].sfn === "b") 
-                    ctx.fillText('\u266D', x-5, y+6);
-                else 
-                    ctx.fillText('\u266E', x-5, y+6);
-                ctx.fillStyle = '#FFF';
+                // top ledger bars (optional)
+                if (j >= 12) { // draw a ledger bar at 12, 13 (actually at 12), and 14
+                    var j_adj = (j === 13) ? 12 : j;
+                    // var adjusted_index = (j === 13) ? index-1 : index;
+                    // need to recompute in case j changed
+                    x = 140 + (2*nx+5)*index - 2*nx + 6; 
+                    y = props.height - (props.gap/2)*(j_adj+1)
+                    // draw the bar
+                    ctx.fillRect(x, y, 50, props.fill);
+
+                    // reset x, y
+                    x = 140 + (2*nx+5)*index;
+                    y = props.height - (props.gap/2)*(j+1)
+                }
+
+                // add the label (if needed)
+                if (props.notes[i].sfn) {
+                    ctx.font = "20px Georgia";
+                    ctx.fillStyle = '#000';
+                    if (props.notes[i].sfn === "#")
+                        ctx.fillText('\u266F', x-5, y+6);
+                    else if (props.notes[i].sfn === "b") 
+                        ctx.fillText('\u266D', x-5, y+6);
+                    else 
+                        ctx.fillText('\u266E', x-5, y+6);
+                    ctx.fillStyle = '#FFF';
+                }
+
+                ++index;
             }
 
-            // top ledger bars
-            if (y_ind >= 12) { // draw a ledger bar at 12, 13 (actually at 12), and 14
-                y_ind = (y_ind === 13) ? 12 : y_ind;
-                var adjusted_index = (y_ind === 13) ? i-1 : i;
-                // need to recompute in case y_ind changed
-                x = 140 + (2*nx+5)*adjusted_index - 2*nx + 6; 
-                y = props.height - (props.gap/2)*(y_ind+1)
-                // draw the bar
-                ctx.fillRect(x, y, 50, props.fill);
+            if (props.notes[i].staff) {
+                ctx.font = "35px Georgia";
+                y = false;
+                y = (j % 2 === 1);
+                x = props.notes[i].align*15;
+                if (props.notes[i].staff === '#') {
+                    // draw a #
+                    y = (y) ? 10 : 9
+                    ctx.fillText('\u266F', 80 + x, start + y - j*(props.gap/2));
+                    continue;
+                } else {
+                    // draw a b
+                    y = (y) ? 10 : 7
+                    ctx.fillText('\u266D', 80 + x, start + y - j*(props.gap/2));
+                    continue;
+                }
             }
         }
 
         // TODO: move this into the above loop
         // TODO: add the 'align' bit
         // time to loop through the scale and find sharps and flats
-        ctx.font = "35px Georgia";
-        y = false;
-        start = props.height - props.gap/2
-        for (var j in props.notes) {
-            console.log(j)
-            if (props.notes[j].staff) {
-                i = props.notes[j].index; 
-                y = (i % 2 === 1);
-                x = props.notes[j].align*15;
-                if (props.notes[j].staff === '#') {
-                    // draw a #
-                    y = (y) ? 10 : 9
-                    ctx.fillText('\u266F', 80 + x, start + y - i*(props.gap/2));
-                    continue;
-                } else {
-                    // draw a b
-                    y = (y) ? 10 : 7
-                    ctx.fillText('\u266D', 80 + x, start + y - i*(props.gap/2));
-                    continue;
-                }
-            }
-        }
+        
+        // start = props.height - props.gap/2
+        // for (var j in props.notes) {
+        //     console.log(j)
+            
+        // }
 
-    }, [props.fill, props.gap, props.width, props.height, props.notes]); // only do this once
+    }, [props.fill, props.gap, props.width, props.height, props.notes, props.range]); // only do this once
 
     return (
     <div>
@@ -299,10 +301,12 @@ function Staff(props) {
                 (props.range === 2) 
                     ? ((props.mode === "chromatic") ? 1100
                         : (props.mode === "minor") ? 730 
-                            : (props.mode === "major") ? 700 : 700)
-                    : ((props.mode === "chromatic") ? 70
-                        : (props.mode === "minor") ? 630 
-                            : (props.mode === "major") ? 600 : 500)
+                        : (props.mode === "major" || props.mode === "blues") ? 700 
+                        : 550)
+                    : ((props.mode === "chromatic") ? 650
+                        : (props.mode === "minor") ? 500 
+                        : (props.mode === "major" || props.mode === "blues") ? 450 
+                        : 350)
                 }
             />
             <Notes onNoteClick={props.onNoteClick} setNote={setNote}
